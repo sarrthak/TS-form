a20af321b81189eefeceef3003e59cd585b585b1
sarrthak <sarthak.trip99@gmail.com>
2021-07-27 04:08:11 +0530

    Initial Commit

---
+++ Ts-Form/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponentComponent } from './form-component/form-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
